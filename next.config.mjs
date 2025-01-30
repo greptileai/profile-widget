95dd8ac507f8f613abd33a6b3c3ac095ecd0623b
achen2089 <anthony@greptile.com>
2025-01-29 19:06:28 -0800

    add more security header info

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: '*.vercel.app',
+      },
+      {
+        protocol: 'https',
         hostname: 'github.com',
       },
       {

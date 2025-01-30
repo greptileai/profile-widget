60d1ea8e7d976f52d048351814d4fb33fddcb223
achen2089 <anthony@greptile.com>
2025-01-30 09:19:02 -0800

    take out portal root

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-black`}
       >
         {children}
-        <div id="portal-root" />
       </body>
     </html>
   );

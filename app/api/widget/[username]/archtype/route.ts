0fb1746d9c5d759d787e4e72b22d5b66529375da
achen2089 <anthony@greptile.com>
2025-01-31 11:10:24 -0800

    make link brighter

--- app/api/widget/[username]/archtype/route.ts
+++ app/api/widget/[username]/archtype/route.ts
@@ -86,7 +86,7 @@ export async function GET(
         <use xlink:href="#image0_4119_14287" transform="scale(0.000666667)"/>
         </pattern>
         </defs>
-      </svg><text x="720" y="20" fill="#444" font-size="10px">statsforgit.com</text>
+      </svg><text x="720" y="20" fill="#777" font-size="10px">statsforgit.com</text>
 
     </svg>`.trim()
 
